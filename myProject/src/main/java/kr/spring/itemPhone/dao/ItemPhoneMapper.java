9712344fe77901fb0bc7282d854fbb874d5a2325
Sterisk <Sterisk@DESKTOP-9NBU5CQ>
2020-12-08 18:21:16 +0900

    itmePhone mapper spmember -> member로 교체

--- myProject/src/main/java/kr/spring/itemPhone/dao/ItemPhoneMapper.java
+++ myProject/src/main/java/kr/spring/itemPhone/dao/ItemPhoneMapper.java
@@ -11,11 +11,11 @@ import kr.spring.itemPhone.vo.ItemPhoneVO;
 
 public interface ItemPhoneMapper {
 	public List<ItemPhoneVO> selectList(Map<String, Object>map);
-	@Select("SELECT COUNT(*) FROM item_phone p JOIN spmember m ON p.mem_num = m.mem_num")
+	@Select("SELECT COUNT(*) FROM item_phone p JOIN member m ON p.mem_num = m.mem_num")
 	public int selectRowCount(Map<String,Object> map);
 	@Insert("INSERT INTO item_phone (phone_num, phone_name, phone_date, phone_apu, phone_ram, phone_memory, phone_display, phone_company, phone_os, phone_type, phone_titleimg, phone_titleimgname, phone_content, phone_contentimg, phone_contentimgname, mem_num) VALUES (phone_seq.nextval, #{phone_name}, #{phone_date}, #{phone_apu}, #{phone_ram}, #{phone_memory}, #{phone_display}, #{phone_company}, #{phone_os}, #{phone_type}, #{phone_titleimg}, #{phone_titleimgname}, #{phone_content}, #{phone_contentimg}, #{phone_contentimgname}, #{mem_num})")
 	public void insertPhone(ItemPhoneVO phone);
-	@Select("SELECT * FROM item_phone p JOIN spmember m ON p.mem_num=m.mem_num WHERE p.phone_num=#{phone_num}")
+	@Select("SELECT * FROM item_phone p JOIN member m ON p.mem_num=m.mem_num WHERE p.phone_num=#{phone_num}")
 	public ItemPhoneVO selectPhone(Integer phone_num);
 	public void updatePhone(ItemPhoneVO phone);
 	@Delete("DELETE FROM item_phone WHERE phone_num=#{phone_num}")
