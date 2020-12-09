10806849a6b406fe810cbf6271e50a210dfa7235
Sterisk <Sterisk@192.168.219.105>
2020-12-09 18:28:26 +0900

    commentMarketMapper,commentQAMapper spmember-> member로 변경

--- myProject/src/main/java/kr/spring/commentMarket/dao/CommentMarketMapper.java
+++ myProject/src/main/java/kr/spring/commentMarket/dao/CommentMarketMapper.java
@@ -15,7 +15,7 @@ public interface CommentMarketMapper {
 	@Insert("INSERT INTO comment_market(market_comm_num, mem_num, market_comm_content, market_num) VALUES (comment_market_seq.nextval, #{mem_num}, #{market_comm_content}, #{market_num})")
 	public void insertComment(CommentMarketVO marketVO);
 	public List<CommentMarketVO> commentMarketList(Map<String,Object> map);
-	@Select("SELECT COUNT(*) FROM comment_market m JOIN spmember b ON m.mem_num=b.mem_num WHERE market_num=#{market_num}")
+	@Select("SELECT COUNT(*) FROM comment_market m JOIN member b ON m.mem_num=b.mem_num WHERE market_num=#{market_num}")
 	public int selectRowCountReply(Map<String,Object> map);
 	@Update("UPDATE comment_market SET market_comm_content=#{market_comm_content} WHERE market_comm_num=#{market_comm_num}")
 	public void updateComment(CommentMarketVO marketVO);
--- myProject/src/main/java/kr/spring/commentQA/dao/CommentQAMapper.java
+++ myProject/src/main/java/kr/spring/commentQA/dao/CommentQAMapper.java
@@ -14,7 +14,7 @@ public interface CommentQAMapper {
 	@Insert("INSERT INTO comment_qa(qa_comm_num, mem_num, qa_comm_content, qa_num) VALUES (comment_qa_seq.nextval, #{mem_num}, #{qa_comm_content}, #{qa_num})")
 	public void insertComment(CommentQAVO commentQA);
 	public List<CommentQAVO> commentQAList(Map<String,Object> map);
-	@Select("SELECT COUNT(*) FROM comment_qa q JOIN spmember b ON q.mem_num=b.mem_num WHERE qa_num=#{qa_num}")
+	@Select("SELECT COUNT(*) FROM comment_qa q JOIN member b ON q.mem_num=b.mem_num WHERE qa_num=#{qa_num}")
 	public int selectRowCountReply(Map<String,Object> map);
 	@Update("UPDATE comment_qa SET qa_comm_content=#{qa_comm_content} WHERE qa_comm_num=#{qa_comm_num}")
 	public void updateComment(CommentQAVO commentQA);
